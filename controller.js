372785ae591da65082fc7a16985ee63dbeae7f11
matheus rocha dos santos <s.rocha17@alunoescolassociais.org.br>
2024-07-04 09:00:48 -0300

    adiciona valor a function

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
 // VALIDAR ACESSO EM TELA DE LOGIN
 function acessar(){
-    
+    let loginEmail = document.getElementById('loginEmail').Value;
+    let loginSenha = document.getElementById('loginSenha').Value;
 }
