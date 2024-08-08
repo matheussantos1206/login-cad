0beb0c6bb9772447aa02b2efeb658d6783339b4a
matheus rocha dos santos <s.rocha17@alunoescolassociais.org.br>
2024-08-08 09:25:13 -0300

    indentação

--- controller.js
+++ controller.js
@@ -1,35 +1,35 @@
 // VALIDAR ACESSO EM TELA DE LOGIN
-function acessar(){
+function acessar() {
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
 
-    if(!loginEmail || !loginSenha){
+    if (!loginEmail || !loginSenha) {
         alert("Favor preencher todo os campos");
-    }else{
+    } else {
         //alert("campos preenchidos com sucesso");
         window.location.href = 'cadastro.html';
     }
-} 
+}
 
 //FUNÇÃO QUE ARMAZENA EM ARRAY NOME NA TELA DE CADASTRO
 
 var dadosListas = [];
-function salvarUser(){
+function salvarUser() {
     let nomeUser = document.getElementById('nomeUser').value;
 
-    if(nomeUser){
-        dadosListas.push(nomeUser);
+    if (nomeUser) {
+        dadosListas.push(nomeUser)
         console.log(dadosListas);
         document.getElementById('nomeUser').value = "";
-    }else{
+    } else {
         alert("favor informar o nome para o cadastro");
     }
 }
 
 // FUNÇÃO PARA CRIAR LISTA
-function criaLista(){
+function criaLista() {
     let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
-    for(let i = 0; i <= (dadosLista.length-1); i++){
+    for (let i = 0; i <= (dadosLista.length - 1); i++) {
         tabela += "<tr><td>" + dadosLista[i] + "</td><td></td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     }
