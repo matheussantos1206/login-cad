e6a8c44d8a0648d0cd53f2471c9db60d4f301811
matheus rocha dos santos <s.rocha17@alunoescolassociais.org.br>
2024-08-08 10:18:15 -0300

    comentação das funções do js

--- controller.js
+++ controller.js
@@ -1,45 +1,63 @@
 // VALIDAR ACESSO EM TELA DE LOGIN
+// Função chamada 'acessar' que é executada quando um evento é acionado (como um clique em um botão)
 function acessar() {
+    // Obtém o valor do campo de entrada com o id 'loginEmail'
     let loginEmail = document.getElementById('loginEmail').value;
+    // Obtém o valor do campo de entrada com o id 'loginSenha'
     let loginSenha = document.getElementById('loginSenha').value;
 
+    // Verifica se algum dos campos está vazio
     if (!loginEmail || !loginSenha) {
+        // Se algum campo estiver vazio, exibe um alerta informando o usuário para preencher todos os campos
         alert("Favor preencher todo os campos");
     } else {
-        //alert("campos preenchidos com sucesso");
+        // Caso ambos os campos estejam preenchidos, redireciona o usuário para a página 'cadastro.html'
         window.location.href = 'cadastro.html';
     }
 }
 
-//FUNÇÃO QUE ARMAZENA EM ARRAY NOME NA TELA DE CADASTRO
 
+// Cria um array global para armazenar os nomes dos usuários
 var dadosListas = [];
+
+// Função que salva o nome do usuário no array e atualiza a lista na tela
 function salvarUser() {
+    // Obtém o valor do campo de entrada com o id 'nomeUser'
     let nomeUser = document.getElementById('nomeUser').value;
 
+    // Verifica se o campo 'nomeUser' não está vazio
     if (nomeUser) {
-        dadosListas.push(nomeUser)
-        //console.log(dadosListas);
+        // Adiciona o nome ao array 'dadosListas'
+        dadosListas.push(nomeUser);
+        // Atualiza a lista na tela chamando a função 'criaLista'
         criaLista();
+        // Limpa o campo de entrada após salvar o nome
         document.getElementById('nomeUser').value = "";
     } else {
-        alert("favor informar o nome para o cadastro");
+        // Se o campo estiver vazio, exibe uma mensagem de alerta para informar o usuário
+        alert("Favor informar o nome para o cadastro");
     }
 }
 
-// FUNÇÃO PARA CRIAR LISTA
+// Função que cria e atualiza a lista na tela com os nomes dos usuários armazenados
 function criaLista() {
-    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+    // Inicializa o conteúdo da tabela com cabeçalhos
+    let tabela = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+
+    // Itera sobre o array 'dadosListas' para criar uma linha para cada nome
     for (let i = 0; i <= (dadosListas.length - 1); i++) {
+        // Adiciona uma nova linha à tabela com o nome e um botão para editar
         tabela += "<tr><td>" + dadosListas[i] + "</td><td> <button type='button' onclick='editar(this.parentNode.parentNode.rowIndex)' class='btn btn-warning'>Editar</button> </td></tr>";
-        document.getElementById('tabela').innerHTML = tabela;
     }
+
+    // Atualiza o conteúdo da tabela no DOM com a nova lista
+    document.getElementById('tabela').innerHTML = tabela;
 }
 
-
-//FUNÇÃO EDITA O NOME DA LISTA 
-
-function editar(i){
-     document.getElementById('nomeUser').value = dadosListas[(i -1)];
-     dadosListas.splice(dadosListas[(i - 1)], 1);
+// Função que edita o nome na lista
+function editar(i) {
+    // Define o valor do campo de entrada 'nomeUser' para o nome a ser editado
+    document.getElementById('nomeUser').value = dadosListas[(i - 1)];
+    // Remove o nome do array 'dadosListas' (esse comando está incorreto, o correto seria usar dadosListas.splice(i - 1, 1))
+    dadosListas.splice(dadosListas[(i - 1)], 1);
 }
